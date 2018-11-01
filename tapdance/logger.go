2d5234726c2db803158b5f6f01d644acadfc2d1b
Sergey Frolov <sergey.frolov@colorado.edu>
2018-11-01 09:51:02 -0600

    Print branch-hash only if it was set externally

--- tapdance/logger.go
+++ tapdance/logger.go
@@ -19,14 +19,17 @@ var initLoggerOnce sync.Once
 
 // Logger is an access point for TapDance-wide logger
 func Logger() *logrus.Logger {
-	const build string = "[BUILD]"
-
 	initLoggerOnce.Do(func() {
 		logrusLogger = logrus.New()
 		logrusLogger.Formatter = new(formatter)
 		logrusLogger.Level = logrus.InfoLevel
-		//logrusLogger.Level = logrus.DebugLevel
-		logrusLogger.Infof("Running gotapdance build %s", build)
+		// logrusLogger.Level = logrus.DebugLevel
+
+		// CI will overwrite the build string with branch and commit hash
+		const build string = "[BUILD]"
+		if build != "[BUILD]" {
+			logrusLogger.Infof("Running gotapdance build %s", build)
+		}
 	})
 	return logrusLogger
 }
